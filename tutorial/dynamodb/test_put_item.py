15c81a33035a801eb4544d9503c907b682863ff2
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-07-12 23:48:21 +0200

    Added tests case for updating document in dynamodb.

--- tutorial/dynamodb/test_put_item.py
+++ tutorial/dynamodb/test_put_item.py
@@ -39,6 +39,45 @@ class TestPutRecord(unittest.TestCase):
         self.assertEqual(item['username'], 'janedoe', "User name should be correct")
         self.assertEqual(item['last_name'], 'Doe', "User surname should be correct")
 
+    def test_update_item(self):
+        # given
+        table = self.dynamodb.Table('users')
+        table.put_item(
+            Item={
+                'username': 'johndoe',
+                'first_name': 'John',
+                'last_name': 'Doe',
+                'age': 32,
+                'account_type': 'standard_user',
+            }
+        )
+
+        # when
+        table.update_item(
+            Key={
+                'username': 'johndoe',
+                'last_name': 'Doe'
+            },
+            UpdateExpression='SET age = :val1',
+            ExpressionAttributeValues={
+                ':val1': 27
+            }
+        )
+
+        # then
+        response = table.get_item(
+            Key={
+                'username': 'johndoe',
+                'last_name': 'Doe'
+            }
+        )
+        item = response['Item']
+        print(item)
+        self.assertEqual(item['username'], 'johndoe', "User name should be correct")
+        self.assertEqual(item['last_name'], 'Doe', "User surname should be correct")
+        self.assertEqual(item['age'], 27, "User age should be correct")
+        self.assertEqual(item['account_type'], 'standard_user', "User account_type should be correct")
+
 
 if __name__ == '__main__':
     unittest.main()
