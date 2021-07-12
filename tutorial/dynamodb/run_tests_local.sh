f0ae04bb0095064b6c3b4331059085cb2332ec8d
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-07-12 21:06:16 +0200

    Removed checking in loop via aws cli if docker image for dynamodb works.

--- tutorial/dynamodb/run_tests_local.sh
+++ tutorial/dynamodb/run_tests_local.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+function waitUntilDockerContainerIsReady {
+    checkCount=1
+    timeoutInSeconds=30
+    while : ; do
+        set +e
+        ./test_docker_image.sh
+        [[ "$?" -ne 0 && $checkCount -ne $timeoutInSeconds ]] || break
+        checkCount=$(( checkCount+1 ))
+        echo "Waiting $checkCount seconds for database to start"
+        sleep 1
+    done
+    set -e
+}
+
 function shutdownDockerContainer {
     lastCommandStatus="$?"
     echo "Shutting down docker container"
@@ -20,7 +34,9 @@ export AWS_SECRET_ACCESS_KEY='DUMMYEXAMPLEKEY'
 export DYNAMODB_PORT=9000
 export DYNAMODB_HOST=localhost
 
+waitUntilDockerContainerIsReady
 python3 -m pip install -r requirements.txt
+
 
 ## Run specific tests
 python3 -m unittest test_create_table.py
