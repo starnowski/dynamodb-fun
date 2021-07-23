0a818704a194ca3ee5fd172b8fd221bc2e5d2f80
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-07-23 13:00:39 +0200

    Added assertions.

--- flaskapp-tests/test_crud_lead_item.py
+++ flaskapp-tests/test_crud_lead_item.py
@@ -18,11 +18,15 @@ class TestCreateTable(unittest.TestCase):
 
         # then
         print('Response for lead object %s ' % response)
-        print('Response json %s ' % response.json())
+        json = response.json()
+        print('Response json %s ' % json)
         self.assertEqual(response.status_code, 200, "Response should have status 200")
-        self.assertTrue('name' in response.json(), "The name should be part of response")
-        self.assertTrue('type' in response.json(), "The type should be part of response")
-        self.assertTrue('url' in response.json(), "The type should be part of response")
+        self.assertTrue('name' in json, "The name should be part of response")
+        self.assertTrue('type' in json, "The type should be part of response")
+        self.assertTrue('url' in json, "The type should be part of response")
+        self.assertEqual(json['name'], 'warehouse', "The item name should be correct")
+        self.assertEqual(json['type'], 'CLOTHES RENTAL', "The item type should be correct")
+        self.assertEqual(json['url'], 'http://warehouse.com/nosuch_address', "The item url should be correct")
 
 
 if __name__ == '__main__':
