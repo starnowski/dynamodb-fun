5bc668c227f169403f452455d526f5fc4e84c447
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-07-27 02:12:58 +0200

    Added tests

--- flaskapp-tests/test_crud_user_stats_item.py
+++ flaskapp-tests/test_crud_user_stats_item.py
@@ -1,6 +1,7 @@
 import unittest
 import os
 import requests
+from datetime import datetime
 
 
 class TestCreateTable(unittest.TestCase):
@@ -11,7 +12,7 @@ class TestCreateTable(unittest.TestCase):
 
     def test_create_lead(self):
         # given
-        payload = {'name': 'warehouse', 'type': 'CLOTHES RENTAL', 'url': 'http://warehouse.com/nosuch_address'}
+        payload = {'user_id': '1', 'timestamp': datetime.utcnow().isoformat(), 'weight': 83, 'blood_pressure': 123}
 
         # when
         response = requests.post(self.host + '/leads', json=payload)
@@ -21,12 +22,13 @@ class TestCreateTable(unittest.TestCase):
         json = response.json()
         print('Response json %s ' % json)
         self.assertEqual(response.status_code, 200, "Response should have status 200")
-        self.assertTrue('name' in json, "The name should be part of response")
-        self.assertTrue('type' in json, "The type should be part of response")
-        self.assertTrue('url' in json, "The type should be part of response")
-        self.assertEqual(json['name'], 'warehouse', "The item name should be correct")
-        self.assertEqual(json['type'], 'CLOTHES RENTAL', "The item type should be correct")
-        self.assertEqual(json['url'], 'http://warehouse.com/nosuch_address', "The item url should be correct")
+        self.assertTrue('user_id' in json, "The user_id should be part of response")
+        self.assertTrue('timestamp' in json, "The timestamp should be part of response")
+        self.assertTrue('weight' in json, "The weight should be part of response")
+        self.assertTrue('blood_pressure' in json, "The blood_pressure should be part of response")
+        self.assertEqual(json['user_id'], '1', "The item user_id should be correct")
+        self.assertEqual(json['weight'], '83', "The item weight should be correct")
+        self.assertEqual(json['blood_pressure'], '123', "The item blood_pressure should be correct")
 
 
 if __name__ == '__main__':
