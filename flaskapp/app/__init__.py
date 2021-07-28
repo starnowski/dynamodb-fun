535bcac93fbf5c271dfebfcc01602a1f4628594a
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-07-28 18:25:46 +0200

    Fixed encoding

--- flaskapp/app/__init__.py
+++ flaskapp/app/__init__.py
@@ -1,6 +1,9 @@
 import os
 import boto3
-from flask import Flask
+from flask import Flask, jsonify
+from flask.json import JSONEncoder
+import calendar
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -13,6 +16,34 @@ def setup_app(app):
     print("flask database host %s " % host)
     print("flask database port %s " % port)
     app.dynamodb = boto3.resource('dynamodb', endpoint_url='http://' + host + ':' + port)
+
+
+class CustomJSONEncoder(JSONEncoder):
+
+    def default(self, obj):
+        try:
+            if isinstance(obj, datetime):
+                if obj.utcoffset() is not None:
+                    obj = obj - obj.utcoffset()
+                millis = int(
+                    calendar.timegm(obj.timetuple()) * 1000 +
+                    obj.microsecond / 1000
+                )
+                return millis
+            if isinstance(o, decimal.Decimal):
+                # wanted a simple yield str(o) in the next line,
+                # but that would mean a yield on the line with super(...),
+                # which wouldn't work (see my comment below), so...
+                return (str(o) for o in [o])
+            iterable = iter(obj)
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
+        return JSONEncoder.default(self, obj)
+
+
+app.json_encoder = CustomJSONEncoder
 
 
 setup_app(app)
