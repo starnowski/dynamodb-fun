73a162032dc786ad761147aa555a4f91a97b84a8
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-08-06 02:48:36 +0200

    Running flask app not only from localhost

--- flaskapp/dynamoapp.py
+++ flaskapp/dynamoapp.py
@@ -1,1 +1,9 @@
 from app import app
+
+
+def main():
+    app.run(host='0.0.0.0', debug=True, threaded=True)
+
+
+if __name__ == "__main__":
+    main()
--- flaskapp/run_app.sh
+++ flaskapp/run_app.sh
@@ -2,4 +2,5 @@
 
 python3 -m pip install -r requirements.txt
 export FLASK_APP=dynamoapp.py
-flask run
+#flask run
+python3 dynamoapp.py
