961d6251d82ec5bb8e5aec12c03e66a2ebd44f5e
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-08-07 18:03:56 +0200

    Added separate script for running application locally with python 3.8 and local dynamodb.

--- flaskapp/run_app_with_python_3-8_local_dynamodb.sh
+++ flaskapp/run_app_with_python_3-8_local_dynamodb.sh
@@ -4,4 +4,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 python3() { python3.8 "$@"; }
 export -f python3
+export DYNAMODB_HOST=localhost
+export DYNAMODB_PORT=9000
 "${SCRIPT_DIR}/run_app.sh"
