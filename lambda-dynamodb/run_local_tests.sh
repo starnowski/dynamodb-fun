4d2f46040d896317018948f24fbbfb6dca236509
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-09-20 22:44:26 +0200

    Added link to stackoverflow

--- lambda-dynamodb/run_local_tests.sh
+++ lambda-dynamodb/run_local_tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ### TODO Copy content form run_local_test.sh
-### TODO How get docker container ip
+### TODO How get docker container ip https://stackoverflow.com/questions/17157721/how-to-get-a-docker-containers-ip-address-from-the-host
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
