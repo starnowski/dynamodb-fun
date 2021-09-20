aa21efd4543ceb5b977b7d5086924cebbf2544dd
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-09-20 22:43:37 +0200

    Added tests script for the lambda docker tests.

---
+++ lambda-dynamodb/run_local_tests.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+### TODO Copy content form run_local_test.sh
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+## Remove database files
+pushd "${SCRIPT_DIR}/.."
+rm -rf "${SCRIPT_DIR}/../docker/dynamodb"
+mkdir -p "${SCRIPT_DIR}/../docker/dynamodb"
+pwd
+docker-compose up --detach
+popd
