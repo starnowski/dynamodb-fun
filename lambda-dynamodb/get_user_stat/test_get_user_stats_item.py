91b4f760a1e4fd879ec04ef6fb45bb9c0afbeb92
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-09-22 22:54:45 +0200

    Added tests

--- lambda-dynamodb/get_user_stat/test_get_user_stats_item.py
+++ lambda-dynamodb/get_user_stat/test_get_user_stats_item.py
@@ -2,36 +2,43 @@ import unittest
 import os
 import requests
 from datetime import datetime, timedelta
+import boto3
 
 
 class TestGetUserStatsItem(unittest.TestCase):
 
     def setUp(self):
-        self.host = os.environ['FLASK_APP_HOST']
+        self.port = os.environ['LAMBDA_PORT']
+        self.host = "localhost:" + self.port
         print("FLASK_APP_HOST host %s " % self.host)
         self.test_start = datetime.utcnow().isoformat()
+        self.user_id = "GET_STAT_LAMBDA_TEST1"
 
     def steps_1_create_user_stat(self):
         # given
-        payload = {'user_id': '1', 'timestamp': datetime.utcnow().isoformat(), 'weight': 83, 'blood_pressure': 123}
+        table = self.dynamodb.Table('users')
 
         # when
-        response = requests.post(self.host + '/user_stats', json=payload)
+        resp = table.put_item(
+            Item={
+                'user_id': user_id,
+                'timestamp': Decimal(datetime.timestamp(self.test_start)),
+                'weight': 82,
+                'blood_pressure': 122
+            }
+        )
 
         # then
-        print('Response for user_stats object %s ' % response)
-        json = response.json()
-        print('Response json %s ' % json)
-        self.assertEqual(response.status_code, 200, "Response should have status 200")
-        self.assertTrue('user_id' in json, "The user_id should be part of response")
-        self.assertTrue('timestamp' in json, "The timestamp should be part of response")
-        self.assertTrue('weight' in json, "The weight should be part of response")
-        self.assertTrue('blood_pressure' in json, "The blood_pressure should be part of response")
-        self.assertEqual(json['user_id'], '1', "The item user_id should be correct")
-        self.assertEqual(json['weight'], 83, "The item weight should be correct")
-        self.assertEqual(json['blood_pressure'], 123, "The item blood_pressure should be correct")
+        response = table.get_item(
+            Key={
+                'username': 'janedoe',
+                'last_name': 'Doe'
+            }
+        )
+        item = response['Item']
+        print(item)
 
-    def steps_2_search_user_stat(self):
+    def steps_2_get_user_stat_vi_lambda(self):
         # given
         payload = {'user_id': '1'}
 
@@ -51,79 +58,6 @@ class TestGetUserStatsItem(unittest.TestCase):
         self.assertEqual(stat['user_id'], '1', "The item user_id should be correct")
         self.assertEqual(stat['weight'], 83, "The item weight should be correct")
         self.assertEqual(stat['blood_pressure'], 123, "The item blood_pressure should be correct")
-
-    def steps_3_search_get_multiple_user_stat(self):
-        # given
-        payload1 = {'user_id': '1', 'timestamp': datetime.utcnow().isoformat(), 'weight': 83, 'blood_pressure': 109}
-        payload2 = {'user_id': '1', 'timestamp': datetime.utcnow().isoformat(), 'weight': 82, 'blood_pressure': 114}
-        response = requests.post(self.host + '/user_stats', json=payload1)
-        self.assertEqual(response.status_code, 200, "Second request should be successful")
-        response = requests.post(self.host + '/user_stats', json=payload2)
-        self.assertEqual(response.status_code, 200, "Third request should be successful")
-        search_payload = {'user_id': '1'}
-
-        # when
-        response = requests.post(self.host + '/user_stats/search', json=search_payload)
-
-        # then
-        print('Response for user_stats search object %s ' % response)
-        json = response.json()
-        print('Response json %s ' % json)
-        self.assertEqual(response.status_code, 200, "Response should have status 200")
-        self.assertTrue('results' in json, "The results should be part of response")
-        stat = json['results'][0]
-        self.assertEqual(len(json['results']), 3, "The array length should be correct")
-        # Sorting should be correct
-        self.assertTrue('timestamp' in stat, "The timestamp should be part of response")
-        self.assertTrue('weight' in stat, "The weight should be part of response")
-        self.assertTrue('blood_pressure' in stat, "The blood_pressure should be part of response")
-        self.assertEqual(stat['user_id'], '1', "The item user_id should be correct")
-        self.assertEqual(stat['weight'], 83, "The item weight should be correct")
-        self.assertEqual(stat['blood_pressure'], 123, "The item blood_pressure should be correct")
-
-    def steps_4_search_get_user_stat_with_limit(self):
-        # given
-        search_payload = {'user_id': '1', 'limit': 1}
-
-        # when
-        response = requests.post(self.host + '/user_stats/search', json=search_payload)
-
-        # then
-        print('Response for user_stats search object %s ' % response)
-        json = response.json()
-        print('Response json %s ' % json)
-        self.assertEqual(response.status_code, 200, "Response should have status 200")
-        self.assertTrue('results' in json, "The results should be part of response")
-        self.assertEqual(len(json['results']), 1, "The array length should be correct")
-
-    def steps_5_search_get_user_stat_after_timestamp(self):
-        # given
-        test_time = datetime.utcnow()
-        after_timestamp = test_time + timedelta(minutes=5)
-        user_stat_timestamp = test_time + timedelta(minutes=15)
-        print ("user_stat_timestamp ", user_stat_timestamp, " after_timestamp ", after_timestamp)
-        payload = {'user_id': '1', 'timestamp': user_stat_timestamp.isoformat(), 'weight': 81, 'blood_pressure': 156}
-        response = requests.post(self.host + '/user_stats', json=payload)
-        self.assertEqual(response.status_code, 200, "Request should be successful")
-        search_payload = {'user_id': '1', 'limit': 1, 'after_timestamp': after_timestamp.isoformat()}
-
-        # when
-        response = requests.post(self.host + '/user_stats/search', json=search_payload)
-
-        # then
-        print('Response for user_stats search object %s ' % response)
-        json = response.json()
-        print('Response json %s ' % json)
-        self.assertEqual(response.status_code, 200, "Response should have status 200")
-        self.assertTrue('results' in json, "The results should be part of response")
-        self.assertEqual(len(json['results']), 1, "The array length should be correct")
-        stat = json['results'][0]
-        self.assertTrue('timestamp' in stat, "The timestamp should be part of response")
-        self.assertTrue('weight' in stat, "The weight should be part of response")
-        self.assertTrue('blood_pressure' in stat, "The blood_pressure should be part of response")
-        self.assertEqual(stat['user_id'], '1', "The item user_id should be correct")
-        self.assertEqual(stat['weight'], 81, "The item weight should be correct")
-        self.assertEqual(stat['blood_pressure'], 156, "The item blood_pressure should be correct")
 
     def _steps(self):
         for name in dir(self): # dir() result is implicitly sorted
