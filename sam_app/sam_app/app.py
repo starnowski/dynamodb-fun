9c512d1e4c2d04b0599a2f864e43468c40773919
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-11-04 23:51:47 +0100

    Fixed setting connection

--- sam_app/sam_app/app.py
+++ sam_app/sam_app/app.py
@@ -41,10 +41,7 @@ def lambda_handler(event, context):
     try:
         if os.getenv("DYNAMODB_HOST") and os.getenv("DYNAMODB_HOST") != "":
             print("DYNAMODB_HOST is " + os.getenv("DYNAMODB_HOST"))
-            dynamodb = boto3.resource('dynamodb', endpoint_url='http://' + os.getenv("DYNAMODB_HOST") + ':9000',
-                                      aws_access_key_id='foo',
-                                      aws_secret_access_key='bar',
-                                      verify=False)
+            dynamodb = boto3.resource('dynamodb', endpoint_url='http://' + os.getenv("DYNAMODB_HOST") + ':9000')
         else:
             dynamodb = boto3.resource('dynamodb')
         if event['resource'] == "/leads" and event['httpMethod'] == "POST":
