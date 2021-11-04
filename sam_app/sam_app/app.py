616b21bbc8bf44ebc2c291f11dfd10a7cbe7b9cc
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-11-04 22:46:32 +0100

    Added endpoints

--- sam_app/sam_app/app.py
+++ sam_app/sam_app/app.py
@@ -1,6 +1,9 @@
 import json
 import boto3
 import os
+from datetime import datetime
+from decimal import Decimal
+
 # import requests
 
 
@@ -66,7 +69,6 @@ def lambda_handler(event, context):
             })
         }
 
-#
     if event['resource'] == "/user_stats" and event['httpMethod'] == "POST":
         data = json.loads(event['body'])  or {}
         print(data)
@@ -100,6 +102,37 @@ def lambda_handler(event, context):
             })
         }
 #
+    if event['resource'] == "/user_stats/search" and event['httpMethod'] == "POST":
+        data = json.loads(event['body'])  or {}
+        print(data)
+        user_id = data.get('user_id')
+        if not user_id:
+            return {"statusCode": 400,
+                    'error': 'Please provide user_id'}
+
+        table = dynamodb.Table('user_stats')
+        primary_key = Key('user_id').eq(user_id)
+        kwargs = {'KeyConditionExpression': primary_key}
+
+        limit = data.get('limit')
+        if limit:
+            kwargs['Limit'] = limit
+
+        after_timestamp = data.get('after_timestamp')
+        if after_timestamp:
+            dt_object = datetime.fromisoformat(after_timestamp)
+            primary_key = primary_key & Key('timestamp').gte(Decimal(datetime.timestamp(dt_object)))
+            kwargs['KeyConditionExpression'] = primary_key
+
+        resp = table.query(**kwargs)
+        print('Search response %s ' % resp['Items'])
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                'results': covert_dynamodb_list(resp['Items'])
+            })
+        }
+#
     return {
         "statusCode": 200,
         "body": json.dumps({
@@ -107,3 +140,13 @@ def lambda_handler(event, context):
             # "location": ip.text.replace("\n", "")
         }),
     }
+
+
+def enrich_with_correct_timestap(item):
+    dt_object = datetime.fromtimestamp(item['timestamp'])
+    item['timestamp'] = dt_object.utcnow().isoformat()
+    return item
+
+
+def covert_dynamodb_list(list):
+    return map(enrich_with_correct_timestap, list)
