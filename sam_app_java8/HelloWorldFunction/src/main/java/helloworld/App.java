11ba699e6262f47a72323df907e46d9dbd054563
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-11-14 11:06:01 +0100

    Added Dao module and fixed App class.

--- sam_app_java8/HelloWorldFunction/src/main/java/helloworld/App.java
+++ sam_app_java8/HelloWorldFunction/src/main/java/helloworld/App.java
@@ -1,5 +1,14 @@
 package helloworld;
 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import helloworld.config.AppModule;
+import helloworld.config.DaggerAppModule;
+
+import javax.inject.Inject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,14 +17,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import javax.inject.Inject;
-
 /**
  * Handler for requests to Lambda function.
  */
@@ -23,6 +24,11 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
 
     @Inject
     ObjectMapper objectMapper;
+
+    public App() {
+        AppModule module = DaggerAppModule.create();
+        module.inject(this);
+    }
 
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
         Map<String, String> headers = new HashMap<>();
--- sam_app_java8/HelloWorldFunction/src/main/java/helloworld/config/DaoModules.java
+++ sam_app_java8/HelloWorldFunction/src/main/java/helloworld/config/DaoModules.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import dagger.Module;
 import dagger.Provides;
 import helloworld.dao.LeadsDao;
+import helloworld.dao.UserStatsDao;
 
 import javax.inject.Singleton;
 
@@ -14,5 +15,11 @@ public class DaoModules {
     @Provides
     public LeadsDao leadsDao(AmazonDynamoDB dynamoDb) {
         return new LeadsDao(dynamoDb);
+    }
+
+    @Singleton
+    @Provides
+    public UserStatsDao userStatsDao(AmazonDynamoDB dynamoDb) {
+        return new UserStatsDao(dynamoDb);
     }
 }
