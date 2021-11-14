d8b06b07c48057c598e607d2eba2460c629aacee
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-11-14 21:50:53 +0100

    Fixed tests execution.

--- sam_app_java8/HelloWorldFunction/src/test/java/helloworld/AppTest.java
+++ sam_app_java8/HelloWorldFunction/src/test/java/helloworld/AppTest.java
@@ -1,10 +1,9 @@
 package helloworld;
 
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
 
 public class AppTest {
   @Test
