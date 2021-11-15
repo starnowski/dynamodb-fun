9bf4a8ed244305b48f3a89e2c393894f0757c3e3
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-11-15 01:20:10 +0100

    Fixed reading environment properties.

--- sam_app_java8/HelloWorldFunction/src/main/java/helloworld/config/AppDynamoDBModule.java
+++ sam_app_java8/HelloWorldFunction/src/main/java/helloworld/config/AppDynamoDBModule.java
@@ -20,7 +20,7 @@ public class AppDynamoDBModule {
         dynamoDBHOst = dynamoDBHOst != null && !dynamoDBHOst.trim().isEmpty() ? dynamoDBHOst : System.getenv("DYNAMODB_HOST");
         if (dynamoDBHOst != null && !dynamoDBHOst.trim().isEmpty()) {
             return AmazonDynamoDBClientBuilder.standard()
-                    .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("FAKE", "FAKE")))
+                    .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("DUMMYIDEXAMPLE", "DUMMYEXAMPLEKEY")))
                     .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://" + dynamoDBHOst + ":" + "9000", "us-east-1"))
                     .build();
         }
