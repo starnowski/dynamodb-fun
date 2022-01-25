00dc32f22cc6ed6802ad839c2249d7fa0fedaa89
starnowski <33316705+starnowski@users.noreply.github.com>
2022-01-26 00:52:28 +0100

    Added database service

--- typescript-dynamodb/src/models/response.ts
+++ typescript-dynamodb/src/models/response.ts
@@ -1,3 +1,4 @@
+import { Any } from "json-schema-to-ts/lib/meta-types";
 
 
 interface IUserStat {
@@ -30,4 +31,29 @@ interface Leads {
     name: string;
     type: string;
     url?: string;
+}
+
+export interface IResponseModel<Type> {
+    body: Type;
+    code: number;
+}
+
+export abstract class AbstractResponseModel<Type> implements IResponseModel<Type> {
+    body: Type;
+    code: number;
+}
+
+export default class ResponseModel extends AbstractResponseModel<any>{
+    body: any;
+    code: number;
+
+    constructor(data = {}, code = 402, message = '') {
+        super();
+        this.body = {
+            data: data,
+            message: message
+        };
+        this.code = code;
+    }
+
 }
---
+++ typescript-dynamodb/src/services/database.services.ts
@@ -0,0 +1,84 @@
+import * as AWS from 'aws-sdk';
+import ResponseModel from 'src/models/response';
+
+// Put
+type PutItem = AWS.DynamoDB.DocumentClient.PutItemInput;
+type PutItemOutput = AWS.DynamoDB.DocumentClient.PutItemOutput;
+
+// Batch write
+type BatchWrite = AWS.DynamoDB.DocumentClient.BatchWriteItemInput;
+type BatchWriteOutPut = AWS.DynamoDB.DocumentClient.BatchWriteItemOutput;
+
+// Update
+type UpdateItem = AWS.DynamoDB.DocumentClient.UpdateItemInput;
+type UpdateItemOutPut = AWS.DynamoDB.DocumentClient.UpdateItemOutput;
+
+// Query
+type QueryItem = AWS.DynamoDB.DocumentClient.QueryInput;
+type QueryItemOutput = AWS.DynamoDB.DocumentClient.QueryOutput;
+
+// Get
+type GetItem = AWS.DynamoDB.DocumentClient.GetItemInput;
+type GetItemOutput = AWS.DynamoDB.DocumentClient.GetItemOutput;
+
+// Delete
+type DeleteItem = AWS.DynamoDB.DocumentClient.DeleteItemInput;
+type DeleteItemOutput = AWS.DynamoDB.DocumentClient.DeleteItemOutput;
+
+type Item = {[index: string]: string};
+
+AWS.config.update({ region: "eu-west-1" });
+
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
+export default class DatabaseService {
+
+    create = async(params: PutItem): Promise<PutItemOutput> => {
+        try {
+            return await documentClient.put(params).promise();
+        } catch (error) {
+            throw new ResponseModel({}, 500, `create-error: ${error}`);
+        }
+    }
+
+    batchCreate = async(params: BatchWrite): Promise<BatchWriteOutPut> => {
+        try {
+            return await documentClient.batchWrite(params).promise();
+        } catch (error) {
+            throw new ResponseModel({}, 500, `batch-write-error: ${error}`);
+        }
+    }
+
+    update = async (params: UpdateItem): Promise<UpdateItemOutPut> => {
+        try {
+            return await documentClient.update(params).promise();
+        } catch (error) {
+            throw new ResponseModel({}, 500, `update-error: ${error}`);
+        }
+    }
+
+    query = async (params: QueryItem): Promise<QueryItemOutput> => {
+        try {
+            return await documentClient.query(params).promise();
+        } catch (error) {
+            throw new ResponseModel({}, 500, `query-error: ${error}`);
+        }
+    }
+
+    get = async (params: GetItem): Promise<GetItemOutput> => {
+        try {
+            return await documentClient.get(params).promise();
+        } catch (error) {
+            throw new ResponseModel({}, 500, `get-error: ${error}`);
+        }
+    }
+
+    delete = async (params: DeleteItem): Promise<DeleteItemOutput> => {
+        try {
+            return await documentClient.delete(params).promise();
+        } catch (error) {
+            throw new ResponseModel({}, 500, `delete-error: ${error}`);
+        }
+    }
+}
+
