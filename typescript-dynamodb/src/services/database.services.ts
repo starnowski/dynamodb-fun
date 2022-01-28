f356cd4d79f56faafc539df71db30056d4747dde
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-01-28 02:19:21 +0100

    Setting region

--- typescript-dynamodb/src/services/database.services.ts
+++ typescript-dynamodb/src/services/database.services.ts
@@ -30,6 +30,7 @@ type DeleteItemOutput = AWS.DynamoDB.DocumentClient.DeleteItemOutput;
 
 const config: IConfig = { region: "eu-west-1" };
 if (process.env.STAGE === process.env.DYNAMODB_LOCAL_STAGE) {
+    config.region = process.env.DYNAMODB_LOCAL_REGION;
     config.accessKeyId = process.env.DYNAMODB_LOCAL_ACCESS_KEY_ID; 
     config.secretAccessKey = process.env.DYNAMODB_LOCAL_SECRET_ACCESS_KEY; 
     config.endpoint = process.env.DYNAMODB_LOCAL_ENDPOINT;
