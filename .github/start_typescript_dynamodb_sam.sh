3f93f67f6015caa7479ba9efa8626a69e1f04ce5
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-02-02 21:14:26 +0100

    Added Github action

---
+++ .github/start_typescript_dynamodb_sam.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+function waitUntilSAMLocalIsReady {
+    checkCount=1
+    timeoutInSeconds=30
+    while : ; do
+        set +e
+        curl http://127.0.0.1:3000
+        [[ "$?" -ne 0 && $checkCount -ne $timeoutInSeconds ]] || break
+        checkCount=$(( checkCount+1 ))
+        echo "Waiting $checkCount seconds for SAM to start"
+        sleep 1
+    done
+    set -e
+}
+tmpfile=$(mktemp)
+ifconfig
+#DYNAMODB_HOST=`/sbin/ip route|awk '/default/ { print $3 }'`
+DYNAMODB_HOST=`ifconfig eth0 | grep "inet\b" | cut -d: -f2 | awk '{print $1;}'`
+DYNAMODB_HOST=`ifconfig eth0 | grep 'inet' | cut -d' ' -f2`
+DYNAMODB_HOST=`ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1`
+cat << SCRIPT > "${tmpfile}"
+{
+  "DynamoDBLeadsFunction": {
+    "DYNAMODB_HOST": "$DYNAMODB_HOST",
+    "AWS_DEFAULT_REGION": "us-east-1",
+    "AWS_ACCESS_KEY_ID": "DUMMYIDEXAMPLE",
+    "AWS_SECRET_ACCESS_KEY": "DUMMYEXAMPLEKEY",
+    "STAGE": "DYNAMODB_LOCAL",
+    "DYNAMODB_LOCAL_STAGE": "DYNAMODB_LOCAL",
+    "DYNAMODB_LOCAL_ACCESS_KEY_ID": "DUMMYIDEXAMPLE",
+    "DYNAMODB_LOCAL_SECRET_ACCESS_KEY": "DUMMYEXAMPLEKEY",
+    "DYNAMODB_LOCAL_ENDPOINT": "$DYNAMODB_HOST",
+    "DYNAMODB_LOCAL_REGION": "${AWS_DEFAULT_REGION}"
+  }
+  ,
+  "DynamoDBUserStatsFunction": {
+    "DYNAMODB_HOST": "$DYNAMODB_HOST",
+    "AWS_DEFAULT_REGION": "us-east-1",
+    "AWS_ACCESS_KEY_ID": "DUMMYIDEXAMPLE",
+    "AWS_SECRET_ACCESS_KEY": "DUMMYEXAMPLEKEY",
+    "STAGE": "DYNAMODB_LOCAL",
+    "DYNAMODB_LOCAL_STAGE": "DYNAMODB_LOCAL",
+    "DYNAMODB_LOCAL_ACCESS_KEY_ID": "DUMMYIDEXAMPLE",
+    "DYNAMODB_LOCAL_SECRET_ACCESS_KEY": "DUMMYEXAMPLEKEY",
+    "DYNAMODB_LOCAL_ENDPOINT": "$DYNAMODB_HOST",
+    "DYNAMODB_LOCAL_REGION": "${AWS_DEFAULT_REGION}"
+  }
+}
+SCRIPT
+echo "${tmpfile}"
+cat "${tmpfile}"
+
+pushd "${SCRIPT_DIR}/../typescript-dynamodb"
+sam local start-api --env-vars "${tmpfile}" &
+popd
+
+waitUntilSAMLocalIsReady
