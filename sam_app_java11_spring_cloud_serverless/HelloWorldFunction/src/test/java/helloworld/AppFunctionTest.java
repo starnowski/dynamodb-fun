0c83f91ad014a5f01a47e1aae00cf21d3db20db2
starnowski <33316705+starnowski@users.noreply.github.com>
2023-02-09 20:38:53 +0100

    Fixed tests [skip ci]

--- sam_app_java11_spring_cloud_serverless/HelloWorldFunction/src/test/java/helloworld/AppFunctionTest.java
+++ sam_app_java11_spring_cloud_serverless/HelloWorldFunction/src/test/java/helloworld/AppFunctionTest.java
@@ -3,7 +3,9 @@ package helloworld;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import helloworld.dao.LeadsDao;
+import helloworld.dao.UserStatsDao;
 import helloworld.model.Leads;
+import helloworld.model.UserStat;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
@@ -28,6 +30,8 @@ public class AppFunctionTest {
     private AmazonDynamoDB dynamoDbAsyncClient;
     @MockBean
     private LeadsDao leadsDao;
+    @MockBean
+    private UserStatsDao userStatsDao;
 
     @Test
     void leads() {
@@ -50,4 +54,29 @@ public class AppFunctionTest {
         // THEN
         value.expectStatus().isOk().expectBody(String.class).isEqualTo("{\"statusCode\":200,\"headers\":{\"X-Custom-Header\":\"application/json\",\"Content-Type\":\"application/json\"},\"multiValueHeaders\":null,\"body\":\"{\\\"name\\\":\\\"warehouse\\\",\\\"type\\\":\\\"CLOTHES RENTAL\\\",\\\"url\\\":\\\"http://warehouse.com/nosuch_address\\\"}\",\"isBase64Encoded\":null}");
     }
+
+    @Test
+    void userStats(){
+        // GIVEN
+        APIGatewayProxyRequestEvent requestEvent = new APIGatewayProxyRequestEvent();
+        requestEvent.setBody("{\"user_id\": \"1\", \"timestamp\": \"2023-02-09T09:25:28.253043\", \"weight\": 83, \"blood_pressure\": 109} ");
+        requestEvent.setHttpMethod("POST");
+        requestEvent.setResource("/user_stats");
+        Mockito.when(userStatsDao.persist(Mockito.any(UserStat.class))).then(new Answer<UserStat>() {
+
+            @Override
+            public UserStat answer(InvocationOnMock invocationOnMock) throws Throwable {
+                return invocationOnMock.getArgument(0);
+            }
+        });
+
+        // WHEN
+        WebTestClient.ResponseSpec value = client.post().uri("/extractPayloadFromGatewayEvent").body(Mono.just(requestEvent), APIGatewayProxyRequestEvent.class).exchange();
+
+        // THEN
+        value.expectStatus().isOk().expectBody(String.class).isEqualTo("{\"statusCode\":200,\"headers\":{\"X-Custom-Header\":\"application/json\",\"Content-Type\":\"application/json\"},\"multiValueHeaders\":null,\"body\":\"{\\\"timestamp\\\":\\\"2023-02-09T09:29:41.000043\\\",\\\"weight\\\":83,\\\"user_id\\\":\\\"1\\\",\\\"blood_pressure\\\":109}\",\"isBase64Encoded\":null}");
+    }
+
+
+    //{'user_id': '1', 'timestamp': '2023-02-09T09:25:28.253043', 'weight': 83, 'blood_pressure': 109}
 }
