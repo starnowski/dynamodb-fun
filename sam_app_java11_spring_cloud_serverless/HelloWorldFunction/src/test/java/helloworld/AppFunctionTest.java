f6bd8b41b395c3f7ba93d9157e0e39dba1c44d3d
starnowski <33316705+starnowski@users.noreply.github.com>
2023-02-09 21:07:08 +0100

    Fixed tests

--- sam_app_java11_spring_cloud_serverless/HelloWorldFunction/src/test/java/helloworld/AppFunctionTest.java
+++ sam_app_java11_spring_cloud_serverless/HelloWorldFunction/src/test/java/helloworld/AppFunctionTest.java
@@ -1,11 +1,13 @@
 package helloworld;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.datamodeling.QueryResultPage;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import helloworld.dao.LeadsDao;
 import helloworld.dao.UserStatsDao;
 import helloworld.model.Leads;
 import helloworld.model.UserStat;
+import helloworld.model.UserStatQueryRequest;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
@@ -18,6 +20,8 @@ import org.springframework.cloud.function.context.test.FunctionalSpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
 
 @FunctionalSpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
@@ -75,6 +79,33 @@ public class AppFunctionTest {
 
         // THEN
         value.expectStatus().isOk().expectBody(String.class).isEqualTo("{\"statusCode\":200,\"headers\":{\"X-Custom-Header\":\"application/json\",\"Content-Type\":\"application/json\"},\"multiValueHeaders\":null,\"body\":\"{\\\"timestamp\\\":\\\"2023-02-09T09:29:41.000043\\\",\\\"weight\\\":83,\\\"user_id\\\":\\\"1\\\",\\\"blood_pressure\\\":109}\",\"isBase64Encoded\":null}");
+    }
+
+    @Test
+    void userStatsSearch(){
+        // GIVEN
+        APIGatewayProxyRequestEvent requestEvent = new APIGatewayProxyRequestEvent();
+        requestEvent.setBody("{\"user_id\": \"3\", \"after_timestamp\": \"2023-02-09T09:25:28.253043\", \"limit\": 2} ");
+        requestEvent.setHttpMethod("POST");
+        requestEvent.setResource("/user_stats/search");
+        Mockito.when(userStatsDao.query(Mockito.any(UserStatQueryRequest.class))).then(new Answer<QueryResultPage<UserStat>>() {
+
+            @Override
+            public QueryResultPage<UserStat> answer(InvocationOnMock invocationOnMock) throws Throwable {
+                UserStatQueryRequest request = invocationOnMock.getArgument(0);
+                QueryResultPage<UserStat> resultPage = new QueryResultPage<UserStat>();
+                UserStat userStat = new UserStat();
+                userStat.setUserId("3");
+                resultPage.setResults(Arrays.asList(userStat, UserStat.builder().userId("3").bloodPressure(131).build()));
+                return resultPage;
+            }
+        });
+
+        // WHEN
+        WebTestClient.ResponseSpec value = client.post().uri("/extractPayloadFromGatewayEvent").body(Mono.just(requestEvent), APIGatewayProxyRequestEvent.class).exchange();
+
+        // THEN
+        value.expectStatus().isOk().expectBody(String.class).isEqualTo("{\"statusCode\":200,\"headers\":{\"X-Custom-Header\":\"application/json\",\"Content-Type\":\"application/json\"},\"multiValueHeaders\":null,\"body\":\"{\\\"results\\\":[{\\\"timestamp\\\":null,\\\"weight\\\":null,\\\"user_id\\\":\\\"3\\\",\\\"blood_pressure\\\":null},{\\\"timestamp\\\":null,\\\"weight\\\":null,\\\"user_id\\\":\\\"3\\\",\\\"blood_pressure\\\":131}]}\",\"isBase64Encoded\":null}");
     }
 
 
