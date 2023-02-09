d3488ef59c338bfea2db6c946064e39b7f4e039c
starnowski <33316705+starnowski@users.noreply.github.com>
2023-02-09 19:18:40 +0100

    Fixed tests [skip ci]

--- sam_app_java11_spring_cloud_serverless/HelloWorldFunction/src/test/java/helloworld/AppFunctionTest.java
+++ sam_app_java11_spring_cloud_serverless/HelloWorldFunction/src/test/java/helloworld/AppFunctionTest.java
@@ -1,7 +1,13 @@
 package helloworld;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import helloworld.dao.LeadsDao;
+import helloworld.model.Leads;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,10 +26,28 @@ public class AppFunctionTest {
     private WebTestClient client;
     @MockBean
     private AmazonDynamoDB dynamoDbAsyncClient;
+    @MockBean
+    private LeadsDao leadsDao;
 
     @Test
-    void doesContainsCloud() {
-        client.post().uri("/extractPayloadFromGatewayEvent").body(Mono.just("this is a cloud"), String.class).exchange()
-                .expectStatus().isOk().expectBody(String.class).isEqualTo("true");
+    void leads() {
+        // GIVEN
+        APIGatewayProxyRequestEvent requestEvent = new APIGatewayProxyRequestEvent();
+        requestEvent.setBody("{\"name\": \"warehouse\", \"type\": \"CLOTHES RENTAL\", \"url\": \"http://warehouse.com/nosuch_address\"}");
+        requestEvent.setHttpMethod("POST");
+        requestEvent.setResource("/leads");
+        Mockito.when(leadsDao.persist(Mockito.any(Leads.class))).then(new Answer<Leads>() {
+
+            @Override
+            public Leads answer(InvocationOnMock invocationOnMock) throws Throwable {
+                return invocationOnMock.getArgument(0);
+            }
+        });
+
+        // WHEN
+        WebTestClient.ResponseSpec value = client.post().uri("/extractPayloadFromGatewayEvent").body(Mono.just(requestEvent), APIGatewayProxyRequestEvent.class).exchange();
+
+        // THEN
+        value.expectStatus().isOk().expectBody(String.class).isEqualTo("{\"statusCode\":200,\"headers\":{\"X-Custom-Header\":\"application/json\",\"Content-Type\":\"application/json\"},\"multiValueHeaders\":null,\"body\":\"{\\\"name\\\":\\\"warehouse\\\",\\\"type\\\":\\\"CLOTHES RENTAL\\\",\\\"url\\\":\\\"http://warehouse.com/nosuch_address\\\"}\",\"isBase64Encoded\":null}");
     }
 }
