76e5c76e205a0c9daa5d093f05ac0b83876a1766
starnowski <33316705+starnowski@users.noreply.github.com>
2023-02-14 00:17:11 +0100

    Changed dateformat

--- sam_app_java11_spring_cloud_serverless/HelloWorldFunction/src/main/java/helloworld/config/MapperModule.java
+++ sam_app_java11_spring_cloud_serverless/HelloWorldFunction/src/main/java/helloworld/config/MapperModule.java
@@ -8,9 +8,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MapperModule {
     @Bean
-    ObjectMapper objectMapper() {
+    public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
         mapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
+        mapper.disable(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS);
         return mapper;
     }
 }
--- sam_app_java11_spring_cloud_serverless/HelloWorldFunction/src/main/java/helloworld/model/UserStatDto.java
+++ sam_app_java11_spring_cloud_serverless/HelloWorldFunction/src/main/java/helloworld/model/UserStatDto.java
@@ -2,6 +2,8 @@ package helloworld.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.deser.std.DateDeserializers;
 
 import java.util.Date;
 
@@ -10,6 +12,7 @@ public class UserStatDto {
     private String userId;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
+    @JsonDeserialize(using = DateDeserializers.DateDeserializer.class)
     private Date timestamp;
     private Integer weight;
 
