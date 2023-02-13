3a8bb56f6e7e5025d22c4cf00b037d7a2808b2b2
starnowski <33316705+starnowski@users.noreply.github.com>
2023-02-13 21:15:22 +0100

    Revert "Fixed mapping"
    This reverts commit 58efa665858e8345b4e68d1c13e6264eaad50d51.

--- sam_app_java11_spring_cloud_serverless/HelloWorldFunction/src/main/java/helloworld/config/MapperModule.java
+++ sam_app_java11_spring_cloud_serverless/HelloWorldFunction/src/main/java/helloworld/config/MapperModule.java
@@ -2,11 +2,6 @@ package helloworld.config;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.json.JsonMapper;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.joda.JodaModule;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,14 +9,8 @@ import org.springframework.context.annotation.Configuration;
 public class MapperModule {
     @Bean
     ObjectMapper objectMapper() {
-        ObjectMapper mapper = JsonMapper.builder() // or different mapper for other format
-                .addModule(new ParameterNamesModule())
-                .addModule(new Jdk8Module())
-                .addModule(new JavaTimeModule())
-                .addModule(new JodaModule())
-                // and possibly other configuration, modules, then:
-                .build();
-        mapper = mapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
         return mapper;
     }
 }
