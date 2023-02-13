60773a6cf682687fd1e84553b26ddcd2f0d4f9a2
starnowski <33316705+starnowski@users.noreply.github.com>
2023-02-14 00:28:19 +0100

    Fixed date deserializer

--- sam_app_java11_spring_cloud_serverless/HelloWorldFunction/src/main/java/helloworld/model/UserStatQueryDto.java
+++ sam_app_java11_spring_cloud_serverless/HelloWorldFunction/src/main/java/helloworld/model/UserStatQueryDto.java
@@ -2,6 +2,8 @@ package helloworld.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.deser.std.DateDeserializers;
 import lombok.Data;
 
 import java.util.Date;
@@ -12,6 +14,7 @@ public class UserStatQueryDto {
     @JsonProperty("user_id")
     private String userId;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
+    @JsonDeserialize(using = DateDeserializers.DateDeserializer.class)
     private Date after_timestamp;
     private Long limit;
 }
